f00300ade291c5642f42d3c6978c7be4676b7cee
Joseph Howard <70383367+jhoward39@users.noreply.github.com>
2022-12-07 22:47:55 -0500

    Update assignment5.asm

--- assignment5.asm
+++ assignment5.asm
@@ -1,9 +1,9 @@
 #Joseph Howard Assignment 5
 #RAT
 # $s0 is MAX
-# $t0 is address of MyArray[index]
-# $t1 is index
-# $t4 is total
+# $s1 is address of MyArray[index] 
+# $s2 is index 
+# $s3 is total 
 
 
 .data
@@ -14,9 +14,9 @@
 	total: .word 0
 .text
 	lw $s0, MAX
-	lw $t1, index
-	la $t0, MyArray
-	lw $t4, total
+	lw $s2, index
+	la $s1, MyArray
+	lw $s3, total
 	
 	li $v0, 1 #print integers
 
@@ -25,18 +25,18 @@
 whileloop:
 	bge $t1, $s0, endwhile #(index<MAX)
 	
-	lw $t2, 0($t0)
+	lw $t2, 0($s1)
 	addi $t3, $zero, 8
-	mult $t4, $t3
+	mult $s3, $t3
 	mflo $t5  #$t5 holds result of total*8
 	
-	add $t4, $zero, $t5 #total = total*8
+	add $s3, $zero, $t5 #total = total*8
 	
-	lw $t6, 0($t0) #t6 holds MyArray[index]
-	add $t4, $t4, $t6 #total = total + MyArray[index]
+	lw $t6, 0($s1) #t6 holds MyArray[index]
+	add $s3, $s3, $t6 #total = total + MyArray[index]
 	
-	addi $t0, $t0, 4 #iterate one element in Myarray
-	addi, $t1, $t1, 1 #add 1 to the index
+	addi $s1, $s1, 4 #iterate one element in Myarray
+	addi, $s2, $s2, 1 #add 1 to the index
 	
 	j whileloop
 	
